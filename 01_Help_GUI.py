14a4281ab0b63e242a2e7a5d24b4a6e6a1d09d25
IssacMathai <issacmathai@tnt.ac.nz>
2021-04-12 12:12:59 +1200

    Create 01_Help_GUI.py

---
+++ 01_Help_GUI.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter(root)
+    root.mainloop()
