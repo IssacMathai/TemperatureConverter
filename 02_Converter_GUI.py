50c722f0d1dbc2b495b58d890c375e36451284ea
IssacMathai <issacmathai@tnt.ac.nz>
2021-05-07 10:09:13 +1200

    Working Converter
    End of Temperature Converter Part 3

--- 02_Converter_GUI.py
+++ 02_Converter_GUI.py
@@ -10,23 +10,23 @@ class Converter:
         background_color = "light blue"
 
         #Converter Frame
-        self.converter_frame = Frame(width=300, bg=background_color,
+        self.converter_frame = Frame(bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
 
         #Temperature Converter Heading (row 0)
         self.temp_heading_label = Label(self.converter_frame,
                                         text="Temperature Converter",
-                                            font="Arial 16 bold",
-                                            bg=background_color,
+                                            font="Arial 19 bold", bg=background_color,
                                             padx=10, pady=10)
         self.temp_heading_label.grid(row=0)
+
         #User instructions (row 1)
         self.temp_instructions_label = Label(self.converter_frame,
                                             text="Type in the amount to be "
                                                  "converted and then push "
                                                  "one of the buttons below...",
-                                        font="Arial 10 italic", wrap=250,
+                                        font="Arial 10 italic", wrap=290,
                                         justify=LEFT, bg=background_color,
                                         padx=10, pady=10)
         self.temp_instructions_label.grid(row=1)
@@ -70,8 +70,8 @@ class Converter:
                                        text="Help", width=5)
         self.help_button.grid(row=0, column=1)
 
-    def temp_convert(self, to):
-        print(to)
+    def temp_convert(self, low):
+        print(low)
 
         error = "#ffafaf" # Pale pink background for when entry box has errors
 
@@ -80,24 +80,48 @@ class Converter:
 
         try:
             to_convert = float(to_convert)
+            has_errors = "no"
             
-            # Check amount is a valid number
+            # Check and convert to Fahrenheit
+            if low == -273 and to_convert >= low:
+                fahrenheit = (to_convert * 9/5) + 32
+                to_convert = self.round_it(to_convert)
+                fahrenheit = self.round_it(fahrenheit)
+                answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
 
-            # Convert to F
-
-            # Convert to C
-
-            # Round!!
+            # Check and convert to Centigrade
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) * 5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees F is {} degrees C".format(to_convert, celsius)
+            
+            else:
+                # Input is invalid (too cold)!
+                answer = "Too Cold!"
+                has_errors = "yes"
 
             # Display answer
-
-            # Add answer to list for History
+            if has_errors == "no":
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
+            # Add Answer to list for History
 
         except ValueError:
-            self.converted_label.configure(text="Enter a number", fg="red")
+            self.converted_label.configure(text="Enter a number!!", fg="red")
             self.to_convert_entry.configure(bg=error)
 
-     
+    def round_it(self, to_round):
+        if to_round % 1 == 0:
+            rounded = int(to_round)
+        else:
+            rounded = round(to_round, 1)
+
+        return rounded
+
 # main routine
 if __name__ == "__main__":
     root = Tk()
